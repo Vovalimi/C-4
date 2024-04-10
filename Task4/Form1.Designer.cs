3d530d88fa373efc534821efc3196e6e21c68443
Vovalimi <146351235+Vovalimi@users.noreply.github.com>
2024-04-10 21:59:59 +0200

    Add files via upload

---
+++ Task4/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Task4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_exit = new System.Windows.Forms.Button();
+            this.button_about = new System.Windows.Forms.Button();
+            this.button_run = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton_buy = new System.Windows.Forms.RadioButton();
+            this.radioButton_sell = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label_necessary = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_exit
+            // 
+            this.button_exit.Location = new System.Drawing.Point(12, 270);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(88, 35);
+            this.button_exit.TabIndex = 0;
+            this.button_exit.Text = "Exit";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // button_about
+            // 
+            this.button_about.Location = new System.Drawing.Point(359, 270);
+            this.button_about.Name = "button_about";
+            this.button_about.Size = new System.Drawing.Size(96, 39);
+            this.button_about.TabIndex = 1;
+            this.button_about.Text = "About";
+            this.button_about.UseVisualStyleBackColor = true;
+            this.button_about.Click += new System.EventHandler(this.button_about_Click);
+            // 
+            // button_run
+            // 
+            this.button_run.Location = new System.Drawing.Point(758, 272);
+            this.button_run.Name = "button_run";
+            this.button_run.Size = new System.Drawing.Size(80, 37);
+            this.button_run.TabIndex = 2;
+            this.button_run.Text = "Run";
+            this.button_run.UseVisualStyleBackColor = true;
+            this.button_run.Click += new System.EventHandler(this.button_run_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 230);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton_buy
+            // 
+            this.radioButton_buy.AutoSize = true;
+            this.radioButton_buy.Location = new System.Drawing.Point(689, 178);
+            this.radioButton_buy.Name = "radioButton_buy";
+            this.radioButton_buy.Size = new System.Drawing.Size(87, 24);
+            this.radioButton_buy.TabIndex = 4;
+            this.radioButton_buy.TabStop = true;
+            this.radioButton_buy.Text = "Купити";
+            this.radioButton_buy.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_sell
+            // 
+            this.radioButton_sell.AutoSize = true;
+            this.radioButton_sell.Location = new System.Drawing.Point(819, 178);
+            this.radioButton_sell.Name = "radioButton_sell";
+            this.radioButton_sell.Size = new System.Drawing.Size(102, 24);
+            this.radioButton_sell.TabIndex = 5;
+            this.radioButton_sell.TabStop = true;
+            this.radioButton_sell.Text = "Продати";
+            this.radioButton_sell.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(619, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Нинішній курс:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(623, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Купівля:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(804, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Продажа:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(623, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(808, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(638, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Сума $ :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(709, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(199, 26);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Navy;
+            this.label5.Location = new System.Drawing.Point(662, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Необхідно (грн):";
+            // 
+            // label_necessary
+            // 
+            this.label_necessary.AutoSize = true;
+            this.label_necessary.Location = new System.Drawing.Point(836, 222);
+            this.label_necessary.Name = "label_necessary";
+            this.label_necessary.Size = new System.Drawing.Size(51, 20);
+            this.label_necessary.TabIndex = 14;
+            this.label_necessary.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 321);
+            this.Controls.Add(this.label_necessary);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton_sell);
+            this.Controls.Add(this.radioButton_buy);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_run);
+            this.Controls.Add(this.button_about);
+            this.Controls.Add(this.button_exit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button button_about;
+        private System.Windows.Forms.Button button_run;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton_buy;
+        private System.Windows.Forms.RadioButton radioButton_sell;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label_necessary;
+    }
+}
---
+++ Task4/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Task4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label_necessary.Enabled = false;
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_about_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.ShowDialog();
+        }
+
+
+        private void button_run_Click(object sender, EventArgs e)
+        {
+            string str_a = textBox1.Text;
+            float buy_k;
+            bool isBuyKValid = float.TryParse(str_a, out buy_k);
+            if (!isBuyKValid)
+            {
+                buy_k = 0;
+                textBox1.Text = "0";
+                textBox1.ForeColor = Color.Red;
+            }
+            else
+            {
+                textBox1.ForeColor = SystemColors.WindowText;
+            }
+
+            string str_b = textBox2.Text;
+            float sele;
+            bool isSeleValid = float.TryParse(str_b, out sele);
+            if (!isSeleValid)
+            {
+                sele = 0;
+                textBox2.Text = "0";
+                textBox2.ForeColor = Color.Red;
+            }
+            else
+            {
+                textBox2.ForeColor = SystemColors.WindowText;
+            }
+
+            string str_c = textBox3.Text;
+            float sum;
+            bool isSumValid = float.TryParse(str_c, out sum);
+            if (!isSumValid)
+            {
+                sum = 100;
+                textBox3.Text = "100";
+                textBox3.ForeColor = Color.Blue;
+            }
+            else
+            {
+                textBox3.ForeColor = SystemColors.WindowText;
+            }
+
+            if (radioButton_buy.Checked)
+            {
+
+                buy_k *= sum;
+                label_necessary.Text = buy_k.ToString();
+            }
+            else if (radioButton_sell.Checked)
+            {
+
+                sele *= sum;
+                label_necessary.Text = sele.ToString();
+            }
+        }
+
+
+    }
+}
